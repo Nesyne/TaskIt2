648234a5dde9b7bebeaeb1de91e8d4dea670cc83
Ryan <RyanBindewald@icloud.com>
2023-06-07 15:59:54 -0700

    Initial Commit

---
+++ TaskIt/TaskItApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskItApp.swift
+//  TaskIt
+//
+//  Created by Ryan Bindewald on 6/7/23.
+//
+
+import SwiftUI
+
+@main
+struct TaskItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
