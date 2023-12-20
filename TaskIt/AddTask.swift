7e5d1f9480733330e2199fb0db57e1dcd0b917e9
Ryan Bindewald <RyanBindewald@gmail.com>
2023-12-19 16:13:36 -0800

    Made it so buttons on adding task are shown at the top

--- TaskIt/AddTask.swift
+++ TaskIt/AddTask.swift
@@ -18,33 +18,54 @@ struct AddTask: View {
     @State var Start = Date()
     @Environment(\.dismiss) var dismiss
     var body: some View {
-        VStack {
-            ScrollView{
-                TextField("Task", text: $tittle1).padding()
-                TextField("Description", text: $bodie1).padding()
-                DatePicker("Start Date", selection: $Start, displayedComponents: [.date, .hourAndMinute]).padding()
-                HStack{
-                    Text("Choose a reminder type")
-                    Picker("Choose a reminder type", selection: $rtype){
-                        ForEach(1..<5){
-                            Text(ty(One: $0))
+        NavigationView{
+            VStack {
+                ScrollView{
+                    TextField("Task Name", text: $tittle1).padding()
+                    TextField("Description", text: $bodie1).padding()
+                    DatePicker("Task Start Date", selection: $Start, displayedComponents: [.date, .hourAndMinute]).padding()
+                    HStack{
+                        Text("Choose a reminder type")
+                        Picker("Choose a reminder type", selection: $rtype){
+                            ForEach(1..<5){
+                                Text(ty(One: $0))
+                            }
                         }
+                        .tint(.blue)
                     }
-                    .tint(.blue)
+              
                 }
-                Button("Add Task"){
-                    if(tittle1 != ""){
-                       let hello = Task(context: top)
-                        hello.id = UUID()
-                        hello.title = tittle1
-                        hello.body = bodie1
-                        hello.done = false
-                        hello.start = Start
-                        hello.cais = Int16(rtype+1)
-                        try? top.save()
+            }
+            
+            .toolbar{
+                ToolbarItem(placement: .navigationBarTrailing){
+                    Button(){
+                        if(tittle1 != ""){
+                            let hello = Task(context: top)
+                            hello.id = UUID()
+                            hello.title = tittle1
+                            hello.body = bodie1
+                            hello.done = false
+                            hello.start = Start
+                            hello.cais = Int16(rtype+1)
+                            try? top.save()
+                            dismiss()
+                        }
+                    } label: {
+                        Text("Add Task")
+                    }
+                }
+                
+                
+                ToolbarItem(placement: .navigationBarLeading){
+                    Button(){
                         dismiss()
+                    } label: {
+                        Text("Cancel")
                     }
                 }
+
+                
             }
         }
     }
